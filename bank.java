fcff082c73050fc1ef6daf808f6441290f2759f6
Hammami Houssem <hammamihossem99@gmail.com>
2022-10-07 19:54:58 +0100

    version 2 with arrays

--- bank.java
+++ bank.java
@@ -2,17 +2,21 @@ import client.Client;
 import compte.Compte;
 
 public class bank {
+    private static Compte[] comptes = new Compte[10];
+    private static Client[] clients = new Client[10];
     public static void main(String[] args) {
+
+        
         // create two clients
-        Client c1 = new Client("Houssem", "12345678");
-        Client c2 = new Client("Omar", "987654321");
+        clients[0] = new Client("Houssem", "12345678");
+        clients[1] = new Client("Omar", "987654321");
 
         // create an account for each client
-        Compte compte1 = new Compte(1000, c1);
-        Compte compte2 = new Compte(459, c2);
+        comptes[0] = new Compte(1000, clients[0]);
+        comptes[1] = new Compte(459, clients[1]);
 
         // show client data
-        System.out.println("name: "+compte1.getClient().getName()+" cin: "+compte1.getClient().getCin()+" balance: "+compte1.getBalance());
-        System.out.println("name: "+compte2.getClient().getName()+" cin: "+compte2.getClient().getCin()+" balance: "+compte2.getBalance());
+        System.out.println("name: "+comptes[0].getClient().getName()+" cin: "+comptes[0].getClient().getCin()+" balance: "+comptes[0].getBalance());
+        System.out.println("name: "+comptes[1].getClient().getName()+" cin: "+comptes[1].getClient().getCin()+" balance: "+comptes[1].getBalance());
     }
 }
